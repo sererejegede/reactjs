5e7953c8af2c09f51504bf453bfedc317ad2682e
Serere Jegede <sererejegede@gmail.com>
2018-06-04 12:40:21 +0100

    Remove inversion

--- src/views/layouts/Navbar.js
+++ src/views/layouts/Navbar.js
@@ -11,8 +11,8 @@ class Navbar extends React.Component {
         const {activeItem} = this.state;
 
         return (
-            <Segment inverted>
-                <Menu inverted pointing secondary>
+            <Segment>
+                <Menu pointing secondary>
                     <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick}/>
                     <Menu.Item name='messages' active={activeItem === 'messages'} onClick={this.handleItemClick}/>
                     <Menu.Item name='friends' active={activeItem === 'friends'} onClick={this.handleItemClick}/>
